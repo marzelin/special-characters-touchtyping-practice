c007aaa0bbf9326a1fde3cfec0d56e7f51d209c5
marzelin <marzelin@gamil.com>
2016-02-27 12:23:23 +0100

    add charactersList, getRandomCharacter

---
+++ script.js
@@ -0,0 +1,29 @@
+var characters = [];
+
+for (var i=0; i < 10; i++) {
+  characters.push(i);
+}
+
+function generateRandomString(charactersList, wordLength, stringLength) {
+  var randomString = '';
+  while (randomString.length < stringLength) {
+  }
+}
+
+function generateRandomWord(charactersList, wordLength) {
+  var randomWord = '';
+  for (var i = 0; i < wordLength; i++) {
+    var randomCharacter = getRandomCharacter(charactersList);
+  }
+}
+
+function getRandomCharacter(charactersList) {
+  var listLength = charactersList.length;
+  var randomIndex = Math.floor(Math.random() * listLength);
+  return charactersList[randomIndex];
+}
+
+// tests ////////////////////////////////////////////////////////////
+
+console.log(characters);
+console.log(getRandomCharacter(characters));
