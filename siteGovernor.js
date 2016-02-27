e4ee5e398c918815e76d679be698973e6569fa88
marzelin <marzelin@gamil.com>
2016-02-27 15:31:32 +0100

    add result field and wire it up

--- siteGovernor.js
+++ siteGovernor.js
@@ -1,11 +1,12 @@
 var randomWordsContainer = document.querySelector('.js-insert-random-words');
 var userInput = document.querySelector('.js-get-user-response');
+var resultsContainer = document.querySelector('.js-show-results');
 
-var listOfCharacters = [1, 2, 3, 4, 5];
+var listOfCharacters = [1, 2, 3, 4, 5, 6];
 var stringGenerator = RandomStringGenerator(listOfCharacters);
-var randomString = stringGenerator.generate(4, 10);
+var randomString = stringGenerator.generate(4, 5);
 
-userInput.addEventListener('keyup', logInput);
+userInput.addEventListener('keyup', showResults);
 
 function logInput(event) {
     if (event.keyIdentifier === 'Enter') {
@@ -14,6 +15,15 @@ function logInput(event) {
     }
 }
 
+function showResults(event) {
+    if (event.keyIdentifier === 'Enter') {
+        if (randomString === userInput.value) {
+        resultsContainer.textContent = 'Good job!';
+        } else {
+        resultsContainer.textContent = 'Try again!';            
+        }
+    }
+}
 
 // tests ///////////////////////////////////////////////////////////////////
 
