354ee0096f5091cafe97959869c4af2e28faa6de
marzelin <marzelin@gamil.com>
2016-02-27 14:32:14 +0100

    add eventListener to input field

--- siteGovernor.js
+++ siteGovernor.js
@@ -1,7 +1,16 @@
 var randomWordsContainer = document.querySelector('.js-insert-random-words');
+var userInput = document.querySelector('.js-get-user-response');
+
 var listOfCharacters = [1, 2, 3, 4, 5];
 var stringGenerator = RandomStringGenerator(listOfCharacters);
 var randomString = stringGenerator.generate(4, 10);
+
+userInput.addEventListener('input', logInput);
+
+function logInput(event) {
+    var inputValue = userInput.value;
+    console.log(inputValue);
+}
 
 
 // tests ///////////////////////////////////////////////////////////////////
