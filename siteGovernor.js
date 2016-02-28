14ca1d8520820eb8817e6e2046656807a96b76e2
marzelin <marzelin@gamil.com>
2016-02-28 16:16:55 +0100

    each word is placed inside of a span element

--- siteGovernor.js
+++ siteGovernor.js
@@ -5,13 +5,14 @@ var resultsContainer = document.querySelector('.js-show-results');
 var listOfCharacters = [1, 2, 3, 4, 5, 6];
 var wordsGenerator = RandomWordsGenerator(listOfCharacters);
 var randomWords = wordsGenerator.generate(4, 5);
-var randomString = randomWords.join(' ');
+var spans = remapWordsToSpanElements(randomWords);
+appendSpans(spans, randomWordsContainer);
 
 userInput.addEventListener('keyup', showResults);
 
 function showResults(event) {
     if (event.keyIdentifier === 'Enter') {
-        if (randomString === userInput.value) {
+        if (randomWordsContainer.textContent === userInput.value) {
         resultsContainer.textContent = 'Good job!';
         } else {
         resultsContainer.textContent = 'Try again!';            
@@ -19,6 +20,25 @@ function showResults(event) {
     }
 }
 
-// tests ///////////////////////////////////////////////////////////////////
+function remapWordsToSpanElements(words) {
+    var spans = words.map((word) => {
+        var span = document.createElement('span');
+        span.textContent = word;
+        return span;
+    });
+    
+    return spans;
+}
 
-randomWordsContainer.textContent = randomString;
+function appendSpans(spans, container) {
+    var lastSpanIndex = spans.length - 1;
+    spans.forEach((span, i) => {
+        container.appendChild(span);
+        if (i !== lastSpanIndex) {
+            var space = document.createTextNode(' ');
+            container.appendChild(space);
+        }
+    });
+}
+
+// tests ///////////////////////////////////////////////////////////////////
